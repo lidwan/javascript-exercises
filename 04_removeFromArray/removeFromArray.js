8571289f29718dc5ba20f5c7b86d26ba3fa8b831
lnidwan <contact@loayidwan.com>
2024-07-23 18:12:20 +0300

    exercise 4 solved passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(A) {
+    for(let i = 0; i < arguments.length; i++){
+        for(let j = 0; j < A.length; j++){
+            while(A[j] === arguments[i]) {
+                A.splice(j, 1);
+            }
+        }
+    }
+    return A;
 };
 
 // Do not edit below this line
