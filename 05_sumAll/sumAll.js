d3d50d0d86d665228f0981a22ab43eb687ed1908
lnidwan <contact@loayidwan.com>
2024-07-23 19:29:38 +0300

    exercise 5 solved. passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if(firstNumber > secondNumber){
+        let tmp = secondNumber;
+        secondNumber = firstNumber;
+        firstNumber = tmp;
+    }
 
+    if(firstNumber < 0 || secondNumber < 0 
+        || !firstNumber || !secondNumber 
+        || typeof(firstNumber) !== "number"|| typeof(secondNumber) !== "number" 
+        || !Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)
+    ){
+        return "ERROR";
+    }
+    else{
+        let sum = 0;
+        for (; firstNumber <= secondNumber; firstNumber++) {
+            sum += firstNumber;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
