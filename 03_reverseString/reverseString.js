8ce27f16e1eeffcdcdebd52303b23fd6f66749bf
lnidwan <contact@loayidwan.com>
2024-07-23 17:51:41 +0300

    exercise 3 solved passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    if(!string)
+        return "";
+
+    const tokenized = string.split("");
+    
+    for (let i = 0, j = tokenized.length - 1; i < tokenized.length / 2; i++, j--) {
+        let tmp = tokenized[i];
+        tokenized[i] = tokenized[j];
+        tokenized[j] = tmp;
+    }
+    return tokenized.join("");
 
 };
 
