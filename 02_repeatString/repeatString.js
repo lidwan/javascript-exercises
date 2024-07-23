4d52f0b9952ea93f85f6ffafbb4bd14e9f6faa3a
lnidwan <contact@loayidwan.com>
2024-07-23 17:13:22 +0300

    exercise 2 solved passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) {
+        return "ERROR";
+    }
+    if(!string)
+        return "";
+    const tmp = string;
+    string = "";
+    while(num > 0){
+        string += tmp;
+        num--;
+    }
+    return string;
 };
 
 // Do not edit below this line
