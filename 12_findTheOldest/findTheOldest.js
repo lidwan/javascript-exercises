d7e36ddb2e1b1c537381926ff010790225a16036
lidwan <contact@loayidwan.com>
2024-07-31 16:26:14 +0300

    all exercises completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+function getAge(death, birth) {
+    if(!death)
+        death = new Date().getFullYear();
+    return death - birth;
+}
 
+const findTheOldest = function(arrOfObjs) {
+return arrOfObjs.reduce((oldest, obj) => {
+    const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const objAge = getAge(obj.yearOfDeath, obj.yearOfBirth)
+    return objAge > oldestAge ? obj : oldest;
+})
 };
 
 // Do not edit below this line
