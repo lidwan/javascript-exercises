e830d27399ebf011335f61e9559eb680916fb4d6
lidwan <contact@loayidwan.com>
2024-07-31 14:15:00 +0300

    10 cmopleted

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+num = parseInt(num);
 
+if(num < 0)
+    return "OOPS"
+if(num === 0)
+    return 0;
+const arr = [1, 1];
+
+for(let i = 2; i <= num - 1; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+}
+return arr[num - 1];
 };
 
 // Do not edit below this line
